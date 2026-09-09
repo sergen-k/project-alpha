96f4241efda5b1d3b68920948158786337d33453
“sergen-k <sergenkaplan249@gmail.com>
2026-09-09 16:02:04 +0200

    Added Battle.cs

---
+++ Battle.cs
@@ -0,0 +1,26 @@
+public class Battle
+{
+    Player Player;
+    Monster Monster;
+
+    public Battle(Player player, Monster monster)
+    {
+        Player = player;
+        Monster = monster;
+    }
+
+    public void InitiateBattle()
+    {
+        
+    }
+
+    public void PlayersTurn()
+    {
+        
+    }
+
+    public void MonstersTurn()
+    {
+        
+    }
+}
