66d923d59984057386995fc80845247db53fda72
mohamed <tmymm026@gmail.com>
2026-09-09 22:25:12 +0200

    Add InventoryManager with basic fields

---
+++ inventory.cs
@@ -0,0 +1,6 @@
+public class InventoryManager
+{
+    public List<Weapon> Weapons = new List<Weapon>();
+    public List<Armour> Armours = new List<Armour>();
+    public List<Potion> Potions = new List<Potion>();
+}
