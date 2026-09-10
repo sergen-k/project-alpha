3b427ba9bef703edf7f578def46be5d56e7fd8f1
yessin129 <jessin129@gmail.com>
2026-09-10 10:31:05 +0200

    added quest class and added game uml

---
+++ Quest.cs
@@ -0,0 +1,18 @@
+public class Quest
+{
+    public int id;
+
+    public string name;
+
+    public string description;
+
+    public Quest(int id,string name, string description)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = descriptionl;
+    }
+
+
+    
+}
