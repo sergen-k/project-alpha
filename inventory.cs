589bb8c53341d33a1ece1bbd147bfa117df04b75
tmymm026-code <tmymm026@gmail.com>
2026-09-14 08:53:59 +0200

    i have added switchweapon option and use potion

--- inventory.cs
+++ inventory.cs
@@ -1,7 +1,5 @@
 public class InventoryManager
 {
-
-    public bool InInventory;
     public List<Weapon> Weapons = new List<Weapon>();
     public List<Armour> Armours = new List<Armour>();
     public List<Potion> Potions = new List<Potion>();
@@ -37,5 +35,50 @@ public class InventoryManager
         Console.WriteLine("Potions:");
         foreach (var potion in Potions)
             Console.WriteLine("- " + potion.Name + " (" + potion.PotionType + ")");
+    }
+
+    // Switch active weapon - only allowed outside of battle
+    public void SwitchWeapon(Weapon weapon, Player player)
+    {
+        if (player.IsFighting)
+        {
+            Console.WriteLine("You cannot switch weapons during battle.");
+            return;
+        }
+
+        if (Weapons.Contains(weapon))
+        {
+            player.CurrentWeapon = weapon;
+            Console.WriteLine("Switched weapon to " + weapon.Name);
+        }
+    }
+
+    // Use a potion - heal potions work anytime, damage potions only in battle
+    public void UsePotion(Potion potion, Player player)
+    {
+        if (!Potions.Contains(potion))
+        {
+            Console.WriteLine("You don't have that potion.");
+            return;
+        }
+
+        if (potion.PotionType == "heal")
+        {
+            player.Heal(potion.Amount); // reuse Player's existing method
+            Potions.Remove(potion);
+            Console.WriteLine("Used potion: " + potion.Name);
+        }
+        else if (potion.PotionType == "damage")
+        {
+            if (!player.IsFighting)
+            {
+                Console.WriteLine("You can only use this potion during battle.");
+                return;
+            }
+
+            // TODO: apply actual damage boost - depends on how Battle.cs tracks bonus damage
+            Potions.Remove(potion);
+            Console.WriteLine("Used potion: " + potion.Name + " (damage boost applied)");
+        }
     }
 }
